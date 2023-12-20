1cc50121225d087a8b9748bfbd77b5a6244959a9
Gustavo Effting <efftinggustavo94@gmail.com>
2023-12-20 11:45:05 -0300

    implement logout

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,28 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap';
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { useLogoutMutation } from '../slices/usersApiSlice';
+import { clearCredentials } from '../slices/authSlice';
 
 const Header = () => {
+  const { userInfo } = useSelector((state) => state.auth)
+  const [logout] = useLogoutMutation()
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await logout().unwrap();
+      dispatch(clearCredentials());
+      navigate('/');
+    } catch (err) {
+      console.log(err?.data?.message || err.error);
+    }
+  }
+
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
@@ -13,16 +33,33 @@ const Header = () => {
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <LinkContainer to='/login'>
-                <Nav.Link>
-                  <FaSignInAlt /> Sign In
-                </Nav.Link>
-              </LinkContainer>
-              <LinkContainer to='/register'>
-                <Nav.Link>
-                  <FaSignOutAlt /> Sign Up
-                </Nav.Link>
-              </LinkContainer>
+              {
+                userInfo ? (
+                  <>
+                    <NavDropdown title={userInfo.name} id='username'>
+                      <LinkContainer to='/profile'>
+                        <NavDropdown.Item>
+                          Profile
+                        </NavDropdown.Item>
+                      </LinkContainer>
+                      <NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
+                    </NavDropdown>
+                  </>
+                ) : (
+                  <>
+                    <LinkContainer to='/login'>
+                      <Nav.Link>
+                        <FaSignInAlt /> Sign In
+                      </Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to='/register'>
+                      <Nav.Link>
+                        <FaSignOutAlt /> Sign Up
+                      </Nav.Link>
+                    </LinkContainer>
+                  </>
+                )
+              }
             </Nav>
           </Navbar.Collapse>
         </Container>
